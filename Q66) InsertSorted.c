49fa060956f283d7a70107bb7c6ccf79f23094db
Veer Meet <vmeet.research@gmail.com>
2025-10-23 17:22:57 +0530

    Create Q66) InsertSorted.c

---
+++ Q66) InsertSorted.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int n,i,a[100],x,j;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)scanf("%d",&a[i]);
+    scanf("%d",&x);
+    for(i=0;i<n&&a[i]<x;i++);
+    for(j=n;j>i;j--)a[j]=a[j-1];
+    a[i]=x;
+    n++;
+    for(i=0;i<n;i++)printf("%d ",a[i]);
+}
