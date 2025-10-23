3fd56958f0fdf6b8685ceb4728b80fbc845a47f9
Veer Meet <vmeet.research@gmail.com>
2025-10-23 17:21:42 +0530

    Create Q61) LinearSearch.c

---
+++ Q61) LinearSearch.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int n,i,a[100],x,f=-1;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)scanf("%d",&a[i]);
+    scanf("%d",&x);
+    for(i=0;i<n;i++)if(a[i]==x)f=i;
+    if(f!=-1)printf("Found at index %d",f);
+    else printf("-1");
+}
