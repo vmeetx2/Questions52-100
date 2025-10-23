376d94974f2d752c8f7c3dae6668bb55759f98cf
Veer Meet <vmeet.research@gmail.com>
2025-10-23 17:09:51 +0530

    Create Q59) EvenOddCount.c

---
+++ Q59) EvenOddCount.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int n,i,a[100],e=0,o=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+        if(a[i]%2==0)e++;
+        else o++;
+    }
+    printf("Even=%d, Odd=%d",e,o);
+}
