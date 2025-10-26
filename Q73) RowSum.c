f4ef86776b861af47a8de548bd13d300637d19fb
Veer Meet <vmeet.research@gmail.com>
2025-10-26 23:58:33 +0530

    Create Q73) RowSum.c

---
+++ Q73) RowSum.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int r,c,i,j,a[100][100],sum[100];
+    scanf("%d%d",&r,&c);
+    for(i=0;i<r;i++)for(j=0;j<c;j++)scanf("%d",&a[i][j]);
+    for(i=0;i<r;i++){
+        sum[i]=0;
+        for(j=0;j<c;j++)sum[i]+=a[i][j];
+    }
+    for(i=0;i<r;i++)printf("%d ",sum[i]);
+}
