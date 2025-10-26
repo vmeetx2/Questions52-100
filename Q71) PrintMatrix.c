83adf9b06c867471d0130a1f7603eb0a79cf1cee
Veer Meet <vmeet.research@gmail.com>
2025-10-26 23:57:59 +0530

    Create Q71) PrintMatrix.c

---
+++ Q71) PrintMatrix.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int r,c,i,j,a[100][100];
+    scanf("%d%d",&r,&c);
+    for(i=0;i<r;i++)for(j=0;j<c;j++)scanf("%d",&a[i][j]);
+    for(i=0;i<r;i++){
+        for(j=0;j<c;j++)printf("%d ",a[i][j]);
+        printf("\n");
+    }
+}
